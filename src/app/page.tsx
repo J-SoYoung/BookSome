34eb563e43d2551e720230e6a8b3be7d31a033a7
thdud <thgml1444@naver.com>
2024-12-02 14:36:19 +0900

    Add new page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,18 @@
+import styles from "./page.module.css";
+
 export default function Home() {
-  return <div>BOOKSOME 책과 썸타는 공간</div>;
+  return (
+    <>
+      <div className={styles.page}>
+        <p>책과 썸타는 공간 BOOKSOME</p>
+        <button >글쓰기</button>
+      </div>
+      <div>
+        <p>이 책을 추천합니다</p>
+      </div>
+      <div>
+        <p>다른 유저들은 이런 책을 읽고 있어요</p>
+      </div>
+    </>
+  );
 }
